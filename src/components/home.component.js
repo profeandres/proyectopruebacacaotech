643b282e944cc38b5374d2817d3abed995e48dbd
Andres Cantillo <andrescantillo@hotmail.com>
2021-10-31 09:16:53 -0600

    pausa para retomar en portatil

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
     export default class Home extends Component {
         render() {
         return (
-        <div>
-            <h1>HOME PAGE</h1>
-            <p>This is the Home Page!</p>
+        <div className="container-sm">
+            
         </div>
         );
     }
--- src/components/products-list.component.js
+++ src/components/products-list.component.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
     export default class ProductList extends Component {
         render() {
         return (
-        <div>
-            <h1> PRODUCT LIST</h1>
-            <p>Products list!</p>
+        <div className="wrapper">
+            <nav></nav>
         </div>
         );
     }
